e898ddad6bdb494a312b2825835b0c547765b4f5
jonahbrawley <jbrawley@redhat.com>
2023-06-07 09:32:24 -0700

    update zr.py with more ideas

--- 00-zero-reconcile/zr.py
+++ 00-zero-reconcile/zr.py
@@ -1,12 +1,29 @@
+import threading # needed for threading
+import time # used to wait
+import curses # used for displaying log on one portion of screen, prompt on other
+
 expected = {
 	"key": "00000000"
 }
 
-current = expected
+current = expected.copy()
 
-def main():
-	print('Starting...')
-	print('Key: ' + current.get('key'))
+def main(stdscr):
+	while true:
+		print('Starting...')
+		print('Key: ' + current.get('key'))
+		userin = input('> ')
+		# user input can be used to generate binary to replace state key
+
+def wellness(stdscr):
+	while true:
+		print('Checking state wellness...')
+		if current.get('key') == "00000000":
+			print('Checks clear')
+		else:
+			print('current.foo.bar state changed blah blah') # diff check dynamically
+			# correct state
+		time.sleep(5)
 
 # generate binary
 def generate_binary(len):
@@ -16,4 +33,4 @@ def generate_binary(len):
 	return binary
 
 if __name__ == '__main__':
-	main()
+	#main(stdscr)
